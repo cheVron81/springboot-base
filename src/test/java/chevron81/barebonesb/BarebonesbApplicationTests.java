b044d10861cff86cf8205dd094604b2f8fa32c49
cheVron81 <peter.seiderer@gmail.com>
2024-11-06 21:35:23 +0100

    initial commit

---
+++ src/test/java/chevron81/barebonesb/BarebonesbApplicationTests.java
@@ -0,0 +1,13 @@
+package chevron81.barebonesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarebonesbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
